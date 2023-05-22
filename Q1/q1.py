a6c894a76b90639590104ed2a7c928f27cb1b7c4
seongwooPark22 <dnflwlq14@konkuk.ac.kr>
2023-05-22 21:07:58 +0900

    second

--- Q1/q1.py
+++ Q1/q1.py
@@ -1,29 +1,35 @@
 import numpy as np
 
+def determinant2by2(a) :
+    return a[0, 0]*a[1, 1]-a[0, 1]*a[1, 0]
+
 def main() :
     vec1 = np.array([1,2,3])
     vec2 = np.array([4,5,6])
 
-    arr = np.array([[1,2],[3,4]])
+    arr = np.array([[1,2], [3,4]])
     eigen_vals, eigen_vectors = np.linalg.eig(arr)
-    determinant = np.linalg.det(arr)
+    determinant = determinant2by2(arr)
+    
+    #determinant = np.linalg.det(arr)
+    #it returns -2.0000000000000004, which is incorect.
+    #to test bigger n*n matrix, please use this
+    
     print("Eigenvalues:", eigen_vals)
     print("Eigenvectors:")
     print(eigen_vectors)
+
     print("Determinant:", determinant)
     
     cross_product = np.cross(vec1, vec2)
-    
     print("Cross product:")
     print(cross_product)
 
-    A = np.array([1,2,-2,2,1,-5,1,-4,1]).reshape(3,3)
+    A = np.array([[1,2,-2],[2,1,-5],[1,-4,1]])
     solution = np.matmul(np.linalg.inv(A), np.array([-15,-21,18]))
     print("Solution:")
     print(solution)
 
-
 if __name__ == "__main__" :
     main()
-
 
--- Q2/q2.py
+++ Q2/q2.py
@@ -7,12 +7,12 @@ def main() :
         [1,1,0,1,0,0]
     ])
     
-    query = [1,1,0,0,1,0]
-    query_norm = np.dot(query, query)
+    query = np.array([1,1,0,0,1,0])
+    query_norm = np.sqrt(np.dot(query, query))
 
     cosine_similarity = []
     for i in docs : 
-        cos_sim_norm = np.dot(i, i)
+        cos_sim_norm = np.sqrt(np.dot(i, i))
         cosine_similarity.append(
             np.dot(i, query)/(query_norm * cos_sim_norm)
         )
